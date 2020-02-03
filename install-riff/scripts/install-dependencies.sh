7878bfc31319568c7fc4c5fe05de1e545cd25b13
Swapnil Bawaskar <sbawaskar@pivotal.io>
2020-02-04 00:03:03 +0530

    wait for k8s to start

--- install-riff/scripts/install-dependencies.sh
+++ install-riff/scripts/install-dependencies.sh
@@ -22,3 +22,6 @@ rm /usr/local/bin/helm
 curl -L https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 
 echo "tools installed!"
+echo "Starting Kubernetes. This may take a few moments, please wait..."
+while [ `minikube status &>/dev/null; echo $?` -ne 0 ]; do sleep 1; done
+echo "Kubernetes Started"
