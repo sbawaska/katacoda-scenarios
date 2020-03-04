cc10f34f48aa3509995167c1e83945257405e962
Swapnil Bawaskar <sbawaskar@pivotal.io>
2020-03-04 07:48:12 -0800

    update builder

--- riff-streaming/scripts/warmup.sh
+++ riff-streaming/scripts/warmup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker pull registry
-docker pull projectriff/builder@sha256:4c8de54865b8958719c21d873645f3b70010f6f37c267bc1c6900b7d0f47eec5
+docker pull projectriff/builder@sha256:69b0ee081b2c776de8ecc36fa9402c003c6b0cc05da342e89ec604b1e3230d10
