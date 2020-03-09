567471783eb33b7e82728766172cd4071afec61e
Swapnil Bawaskar <sbawaskar@pivotal.io>
2020-03-09 15:56:02 -0700

    no prefetch

--- riff-streaming/scripts/warmup.sh
+++ riff-streaming/scripts/warmup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #docker pull registry
-docker pull projectriff/builder@sha256:69b0ee081b2c776de8ecc36fa9402c003c6b0cc05da342e89ec604b1e3230d10
+#docker pull projectriff/builder@sha256:69b0ee081b2c776de8ecc36fa9402c003c6b0cc05da342e89ec604b1e3230d10
